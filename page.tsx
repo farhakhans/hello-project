f11b5b0a8675af17c94036a58687388e8db75cec
typescript project <163310966+farhakhans@users.noreply.github.com>
2024-10-03 23:28:34 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,32 @@
+
+
+export default function Home() {
+  return (
+
+    
+
+
+ 
+ <div>
+  
+  <h1 className="text-red-500 text-center text-8xl font-sans text-bold  bg-fuchsia-400 h-15 font-bold" >  Its My First Project from Farha Khan</h1><br></br>
+
+ <h2 className="text-orange-300 text-center text-8xl font-sans font-bold bg-sky-200 h-35">Amazing color </h2>
+
+ 
+
+
+ </div>
+  );
+}
+
+
+
+ 
+
+
+
+
+
+
+    
